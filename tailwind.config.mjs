9130d2d454ad9f7834da160813f03549374d7be5
degsz <125046749+degszz@users.noreply.github.com>
2024-09-19 20:45:34 -0300

    feat(sidebar): sidebar y retoques

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,31 @@ export default {
 	theme: {
 		extend: {
 			height: {
-				'custom-calc': 'calc(100vh - 160px)', 
-				'custom--calc': 'calc(100vh - 102px)' 
+				'custom-calc': 'calc(100vh - 80px)',
+				'custom--calc': 'calc(100vh - 102px)'
+			},
+			fontFamily: {
+
+			},
+			colors: {
+				'rose': {
+					'50': '#fff0f9',
+					'100': '#ffe3f6',
+					'200': '#ffc6ec',
+					'300': '#ff98db',
+					'400': '#ff58c1',
+					'500': '#ff27a5',
+					'600': '#ff0080',
+					'700': '#df0064',
+					'800': '#b80053',
+					'900': '#980348',
+					'950': '#5f0026',
+				},
+
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	],
 }
